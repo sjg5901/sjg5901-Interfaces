7baf418310738f0ff84e70b086a7253a5c7a98d9
Skyler Giannetti <sjg5901@psu.edu>
2023-07-06 04:30:10 +0000

    Interfaces lab complete

--- src/main/java/SampleClassC.java
+++ src/main/java/SampleClassC.java
@@ -27,6 +27,9 @@ public class SampleClassC implements Comparable<SampleClassC>{
      * @param other another object of type SampleClassC, which we are comparing to 'this' object.
      */
     public int compareTo(SampleClassC other) {
+
+        if (this.val < other.val) return -1;
+        else if (this.val > other.val) return 1;
         return 0;
     }
 
